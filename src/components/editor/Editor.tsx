8799ec6a9a98ddd31f296821f72820b779e32a7d
berdal84 <berenger@42borgata.com>
2024-01-27 23:06:47 -0500

    feat(Editor): first - partial - success to handle of RichText

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import Modal from "@mui/material/Modal";
+import Dialog from "@mui/material/Dialog";
 import EditIcon from "@mui/icons-material/Edit";
 import { ApiClient } from "../../utils/ApiClient";
 import { InitialConfigType, LexicalComposer } from '@lexical/react/LexicalComposer';
@@ -10,16 +10,59 @@ import { ContentEditable } from '@lexical/react/LexicalContentEditable';
 import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
 import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin';
 import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
-import { EditorState } from "lexical";
+import { $createTextNode, $getRoot, EditorState, $createParagraphNode } from 'lexical';
+import { $createHeadingNode, $createQuoteNode } from '@lexical/rich-text';
 import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
 import SubmitButtonPlugin from "../lexical/SubmitButtonPlugin";
 import AutoFocusPlugin from "../lexical/AutoFocusPlugin";
 import { EditorProps } from "./types";
 import { FormattingToolBarPlugin } from "../lexical/FormattingToolBarPlugin";
 import { composerBaseConfig } from "./composerBaseConfig";
+import { DialogContent, DialogTitle } from "@mui/material";
+
+/**
+ * Function to give to composerConfig to initialize editorState
+ */
+function prepopulatedRichText() {
+  const root = $getRoot();
+  if (root.getFirstChild() === null) {
+
+    // Heading Template
+    const heading = $createHeadingNode('h1');
+    heading.append($createTextNode('Your title here...'));
+    root.append(heading);
+
+    // Note Template
+    const quote = $createQuoteNode();
+    quote.append(
+      $createTextNode(
+        'Enter your note here... '
+      ),
+    );
+    root.append(quote);
+    
+    // Can't make the following like work, investigate.
+    // const paragraph = $createParagraphNode();
+    //paragraph.append(
+    //  $createTextNode('Enter your note here... '),
+    //  $createTextNode('@lexical/react').toggleFormat('code'),
+    //  $createTextNode('.'),
+    //  $createTextNode(' Try typing in '),
+    //  $createTextNode('some text').toggleFormat('bold'),
+    //  $createTextNode(' with '),
+    //  $createTextNode('different').toggleFormat('italic'),
+    //  $createTextNode(' formats.'),
+    //);
+    //root.append(paragraph);
+  }
+}
 
 function Editor({ resource }: EditorProps) {
-  const composerConfig: InitialConfigType = { ...composerBaseConfig };
+  const composerConfig: InitialConfigType = {
+    ...composerBaseConfig,
+    editable: true,
+    editorState: prepopulatedRichText
+  };
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -37,7 +80,7 @@ function Editor({ resource }: EditorProps) {
     console.log(`Editor content changed:\n${JSON.stringify(editorState, null, '  ')}`);
     // TODO: validate data
   }
-
+  
   return (
     <>
       <Box sx={{ paddingX: 3, paddingY: 4, borderBottom: 1, borderColor: "grey.300" }}>
@@ -66,20 +109,20 @@ function Editor({ resource }: EditorProps) {
       >
         <DialogTitle>Create a Note</DialogTitle>
         <DialogContent>
-            <LexicalComposer initialConfig={composerConfig}>
-              <FormattingToolBarPlugin />
-              <RichTextPlugin
-                placeholder={null}
+          <LexicalComposer initialConfig={composerConfig}>
+            <FormattingToolBarPlugin />
+            <RichTextPlugin
+              placeholder={null}
               contentEditable={
                 <ContentEditable className="m-2 p-2 border" />
               }
-                ErrorBoundary={LexicalErrorBoundary}
-              />
-              <OnChangePlugin onChange={handleChange} />
-              <HistoryPlugin />
-              <AutoFocusPlugin />
-              <SubmitButtonPlugin onSubmit={handleSubmit} />
-            </LexicalComposer>
+              ErrorBoundary={LexicalErrorBoundary}
+            />
+            <OnChangePlugin onChange={handleChange} />
+            <HistoryPlugin />
+            <AutoFocusPlugin />
+            <SubmitButtonPlugin onSubmit={handleSubmit} />
+          </LexicalComposer>
         </DialogContent>
       </Dialog>
     </>
