a9fe86e84e1e3155d2045f195402671721f73f9b
Bérenger Dalle-Cort <berenger@42borgata.com>
2024-03-06 16:28:29 -0500

    fix(User): Form and Modal works for login

--- src/components/user/UserLoginForm.tsx
+++ src/components/user/UserLoginForm.tsx
@@ -3,12 +3,16 @@ import { UserCredentials } from "../../types/user";
 import { useForm, SubmitHandler } from "react-hook-form"
 import { ApiClient } from "../../utils/ApiClient";
 import { useUserDispatch } from "../../contexts/UserContext";
+import { Box, Button } from "@mui/material";
 
+type Props = {
+    onLogged: () => void;
+}
 
 /**
  * Provides a simple login form with username and password input fields.
  */
-export function UserLoginForm() {
+export function UserLoginForm({ onLogged }: Props) {
 
     const dispatch = useUserDispatch();
 
@@ -18,8 +22,9 @@ export function UserLoginForm() {
         formState: { errors },
     } = useForm<UserCredentials>()
 
-    const onSubmit: SubmitHandler<UserCredentials> = async (credentials) => {
-        console.log(credentials);
+    const onSubmit: SubmitHandler<UserCredentials> = async (credentials, event) => {
+
+        event?.preventDefault();
 
         // TODO: consider grouping these 3 dispatch calls into ApiClient after turning ApiClient into a hook.
         //       An other option would be to use Redux with 3rd party.
@@ -28,21 +33,29 @@ export function UserLoginForm() {
         const connected = await ApiClient.login(credentials);
         if ( connected ) {
             dispatch({ type: 'loggedIn', username: credentials.username})
+            onLogged()
         } else {
             dispatch({ type: 'error', message: "unable to log in"})
         }
+        event?.target.reset();
     }
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            
+        <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col p-2 gap-3"
+        >
+            <h1>Login Form</h1>
             <TextField {...register("username")} required label="Username" variant="standard" />
             {errors.username && <span>This field is required</span>}
 
             <TextField {...register("password")} required label="Password" variant="standard" type="password"/>
             {errors.password && <span>This field is required</span>}
 
-            <input type="submit" />
+            <Box className="flex gap-2 justify-center">
+                <Button type="reset" variant="contained">Reset</Button>
+                <Button type="submit" variant="contained">Submit</Button>
+            </Box>
         </form>
     )
 }
--- src/components/user/UserLoginModal.tsx
+++ src/components/user/UserLoginModal.tsx
@@ -1,13 +1,20 @@
-import Modal, { ModalProps } from "@mui/material/Modal";
 import { UserLoginForm } from "./UserLoginForm";
+import { Box, Dialog } from "@mui/material";
+
+type Props = {
+    open: boolean;
+    onClose: () => void;
+}
 
 /**
  * Wraps UserLoginForm in a Modal
  */
-export function UserLoginModal(props: Pick<ModalProps, 'open' | 'onClose'>) {
+export function UserLoginModal(props: Props) {
     return (
-        <Modal {...props}>
-            <UserLoginForm/>
-        </Modal>
+        <Dialog {...props}>
+            <Box sx={{ width: 550 }}>
+                <UserLoginForm onLogged={props.onClose}/>
+            </Box>
+        </Dialog>
     )
 }
