ef33cd6ccd9503d50a4d557e9f2b4fc6125ec4d8
Bérenger Dalle-Cort <berenger@42borgata.com>
2024-03-06 16:27:59 -0500

    fix(url): isolate try/catch

--- src/utils/Url.ts
+++ src/utils/Url.ts
@@ -1,21 +1,23 @@
 export function formatUrl(url: string): { hostname: string; path: string } {
+
+  let parsedUrl: URL | undefined;
+
   try {
-    const parsedUrl = new URL(url);
-    const hostname = parsedUrl.hostname.replace(/^www\./, "");
-    const pathSegments = parsedUrl.pathname
-      .split("/")
-      .filter((segment) => segment);
-
-    let path = pathSegments.join(" > ");
-    if (pathSegments.length > 0) {
-      path = "> " + path;
-    }
-
-    return { hostname, path };
+    parsedUrl = new URL(url);
   } catch (error) {
-    console.error("Invalid URL:", error);
-    const hostname = "";
-    const path = "";
-    return { hostname, path };
+    console.warn(`"${url}" is not an URL.\n`, error);    
+    return { hostname: '', path: '' };
   }
+
+  const hostname = parsedUrl.hostname.replace(/^www\./, "");
+  const pathSegments = parsedUrl.pathname
+    .split("/")
+    .filter((segment) => segment);
+
+  let path = pathSegments.join(" > ");
+  if (pathSegments.length > 0) {
+    path = "> " + path;
+  }
+
+  return { hostname, path };
 }
